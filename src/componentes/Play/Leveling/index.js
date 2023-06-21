f0de62e2bbe05825e69d04bf9eaf49c9b7186c70
Natália Lima <natalia.lima@prit.app>
2023-06-21 20:49:00 -0300

    Perguntas animadas

--- src/componentes/Play/Leveling/index.js
+++ src/componentes/Play/Leveling/index.js
@@ -2,28 +2,71 @@ import React, { useState } from 'react';
 import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import styles from './styles';
 import Balloon from "react-native-balloon";
-//nivelação 10 perguntas
+import * as Animatable from 'react-native-animatable';
+
+//10 questões
 const questions = [
-  { question: 'Quanto é 7 + 5?',  options: ['10', '11', '12'],   correctAnswer: '12' },
-  { question: 'Quanto é 10 - 2?', options: ['6', '7', '8'],      correctAnswer: '8'  },
-  { question: 'Quanto é 3 x 4?',  options: ['9', '10', '12'],    correctAnswer: '12' },
-  { question: 'Quanto é 18 ÷ 3?', options: ['4', '6', '8'],      correctAnswer: '6'  },
-  { question: 'Quanto é 9 + 6',   options: ['13', '14', '15'],   correctAnswer: '15' },
-  { question: 'Quanto é 5 - 1?',  options: ['2', '3', '4'],      correctAnswer: '4'  },
-  { question: 'Quanto é 2 x 7?',  options: ['12', '13', '14'],   correctAnswer: '14' },
-  { question: 'Quanto é 15 ÷ 5?', options: ['2', '3', '4'],      correctAnswer: '3'  },
-  { question: 'Quanto é 11 + 9?', options: ['18', '19', '20'],   correctAnswer: '20' },
-  { question: 'Quanto é 16 - 8?', options: ['6', '7', '8'],      correctAnswer: '8'  },
+  {
+    images: [
+      {
+        source: require('../../../assets/number1.png'),
+        isCorrect: false
+      },
+      {
+        source: require('../../../assets/number8.png'),
+        isCorrect: true
+      },
+      {
+        source: require('../../../assets/number2.png'),
+        isCorrect: false
+      }
+    ],
+    question: 'Quanto é 4 + 4 ?'
+  },
+  {
+    images: [
+      {
+        source: require('../../../assets/number1.png'),
+        isCorrect: true
+      },
+      {
+        source: require('../../../assets/number8.png'),
+        isCorrect: false
+      },
+      {
+        source: require('../../../assets/number2.png'),
+        isCorrect: false
+      }
+    ],
+    question: 'Quanto é 4 - 3 ?'
+  },
+  {
+    images: [
+      {
+        source: require('../../../assets/number1.png'),
+        isCorrect: false
+      },
+      {
+        source: require('../../../assets/number8.png'),
+        isCorrect: false
+      },
+      {
+        source: require('../../../assets/number2.png'),
+        isCorrect: true
+      }
+    ],
+    question: 'Quanto é 4 - 2 ?'
+  },
+  // Adicione outras perguntas com imagens e respostas corretas correspondentes
 ];
 
-const Leveling = ({navigation}) => {
- 
+const Leveling = ({ navigation }) => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [correctAnswers, setCorrectAnswers] = useState(0);
   const [answerFeedback, setAnswerFeedback] = useState('');
 
-  const handleAnswer = (selectedAnswer) => {
-    if (selectedAnswer === questions[currentQuestion].correctAnswer) {
+  const handleAnswer = (isCorrect) => {
+    if (isCorrect) {
       setCorrectAnswers(correctAnswers + 1);
       setAnswerFeedback('Parabéns! Você acertou!');
     } else {
@@ -32,18 +75,18 @@ const Leveling = ({navigation}) => {
 
     if (currentQuestion + 1 === questions.length) {
       let level = '';
-    
+
       if (correctAnswers >= 8) {
-        navigation.navigate("Hard")
+        navigation.navigate("Hard");
       } else if (correctAnswers >= 5) {
-        navigation.navigate("Medium")
-      } else if (correctAnswers >= 3) {
-        navigation.navigate("Easy")
+        navigation.navigate("Medium");
+      } else if (correctAnswers >= 2) {
+        navigation.navigate("Easy");
       } else {
         level = 'Nível não alcançado';
       }
     }
-    
+
     setCurrentQuestion(currentQuestion + 1);
   };
 
@@ -52,32 +95,45 @@ const Leveling = ({navigation}) => {
 
     return (
       <View style={styles.container}>
-        <Image source={require('../../../assets/happyTony.png')} style={styles.tonyStyle}/>
-        
+        <Animatable.View animation="fadeIn" duration={3000}>
+          <Image source={require('../../../assets/happyTony.png')} style={styles.tonyStyle} />
+        </Animatable.View>
+
         <View style={styles.balloon}>
-          <Balloon 
-            borderColor="#2E86C1"
-            backgroundColor="#D6EAF8"
-            borderWidth={2}
-            borderRadius={20}
-            triangleSize={15}
-            triangleDirection='bottom'
-            triangleOffset='23%'
-          >
-            <Text style={styles.questionText}>{question.question}</Text>
-          </Balloon>
+          <Animatable.View animation="fadeIn" duration={5000}>
+            <Balloon
+              borderColor="#2E86C1"
+              backgroundColor="#D6EAF8"
+              borderWidth={2}
+              borderRadius={20}
+              triangleSize={15}
+              triangleDirection='bottom'
+              triangleOffset='23%'
+            >
+              <Text style={styles.questionText}>{question.question}</Text>
+            </Balloon>
+          </Animatable.View>
         </View>
-        
-        {question.options.map((option, index) => (
-          <TouchableOpacity
-            key={index}
-            style={styles.optionButton}
-            onPress={() => handleAnswer(option)}
-          >
-            <Text style={styles.optionText}>{option}</Text>
-          </TouchableOpacity>
-        ))}
-        {answerFeedback !== '' && <Text style={styles.feedbackText}>{answerFeedback}</Text>}
+
+        <View style={styles.imagesContainer}>
+          {question.images.map((image, index) => (
+            <TouchableOpacity
+              key={index}
+              style={styles.imageButton}
+              onPress={() => handleAnswer(image.isCorrect)}
+            >
+              <Animatable.View animation="bounceIn" duration={2000}>
+                <Image source={image.source} style={styles.image} />
+              </Animatable.View>
+            </TouchableOpacity>
+          ))}
+        </View>
+
+        {answerFeedback !== '' && (
+          <Animatable.View animation="fadeIn" duration={2000}>
+            <Text style={styles.feedbackText}>{answerFeedback}</Text>
+          </Animatable.View>
+        )}
       </View>
     );
   }
