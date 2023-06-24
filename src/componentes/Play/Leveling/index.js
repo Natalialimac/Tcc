b05ed6b625284d9ed76b93262f173929536b2823
Natália Lima <natalia.lima@prit.app>
2023-06-23 22:09:59 -0300

    Telas de nivelamento ok

--- src/componentes/Play/Leveling/index.js
+++ src/componentes/Play/Leveling/index.js
@@ -77,92 +77,92 @@ const questions = [
   {
     images: [
       {
-        source: require('../../../assets/buttons/number1.png'),
-        isCorrect: false
-      },
-      {
-        source: require('../../../assets/buttons/number8.png'),
-        isCorrect: false
-      },
-      {
-        source: require('../../../assets/buttons/number2.png'),
+        source: require('../../../assets/buttons/number4.png'),
         isCorrect: true
+      },
+      {
+        source: require('../../../assets/buttons/number12.png'),
+        isCorrect: false
+      },
+      {
+        source: require('../../../assets/buttons/number9.png'),
+        isCorrect: false
       }
     ],
-    question: 'Quanto é 4 - 2 ?'
+    question: 'Quanto é 8 / 2?'
   },
   {
     images: [
       {
-        source: require('../../../assets/buttons/number1.png'),
+        source: require('../../../assets/buttons/fingers5.png'),
         isCorrect: false
       },
       {
-        source: require('../../../assets/buttons/number8.png'),
+        source: require('../../../assets/buttons/fingers3.png'),
         isCorrect: false
       },
       {
-        source: require('../../../assets/buttons/number2.png'),
+        source: require('../../../assets/buttons/fingers7.png'),
         isCorrect: true
       }
     ],
-    question: 'Quanto é 4 - 2 ?'
+    question: 'Qual número vem depois de 6?'
   },
   {
     images: [
       {
-        source: require('../../../assets/buttons/number1.png'),
+        source: require('../../../assets/buttons/number19.png'),
         isCorrect: false
       },
       {
-        source: require('../../../assets/buttons/number8.png'),
-        isCorrect: false
-      },
-      {
-        source: require('../../../assets/buttons/number2.png'),
+        source: require('../../../assets/buttons/number16.png'),
         isCorrect: true
+      },
+      {
+        source: require('../../../assets/buttons/number12.png'),
+        isCorrect: false
       }
     ],
-    question: 'Quanto é 4 - 2 ?'
+    question: 'Quanto é 3 * 4 + 4 ?'
   },
   {
     images: [
       {
-        source: require('../../../assets/buttons/number1.png'),
+        source: require('../../../assets/buttons/fingers1.png'),
         isCorrect: false
       },
       {
-        source: require('../../../assets/buttons/number8.png'),
-        isCorrect: false
-      },
-      {
-        source: require('../../../assets/buttons/number2.png'),
+        source: require('../../../assets/buttons/fingers3.png'),
         isCorrect: true
+      },
+      {
+        source: require('../../../assets/buttons/fingers5.png'),
+        isCorrect: false
       }
     ],
-    question: 'Quanto é 4 - 2 ?'
+    question: 'Clique no mão com o número 3'
   },
   {
     images: [
       {
-        source: require('../../../assets/buttons/number1.png'),
+        source: require('../../../assets/buttons/number14.png'),
         isCorrect: false
       },
       {
-        source: require('../../../assets/buttons/number8.png'),
-        isCorrect: false
-      },
-      {
-        source: require('../../../assets/buttons/number2.png'),
+        source: require('../../../assets/buttons/number19.png'),
         isCorrect: true
+      },
+      {
+        source: require('../../../assets/buttons/number11.png'),
+        isCorrect: false
       }
     ],
-    question: 'Quanto é 4 - 2 ?'
+    question: 'Quanto é 5 * 5 - 6?'
   },
   {
     images: [
       {
-        source: require('../../../assets/buttons/number1.png'),
+        source: require('../../../assets/buttons/number5.png'),
         isCorrect: false
       },
       {
@@ -170,11 +170,11 @@ const questions = [
         isCorrect: false
       },
       {
-        source: require('../../../assets/buttons/number2.png'),
+        source: require('../../../assets/buttons/number4.png'),
         isCorrect: true
       }
     ],
-    question: 'Quanto é 4 - 2 ?'
+    question: 'Quanto é 20 / 10 * 2 ?'
   },
 ];
 
